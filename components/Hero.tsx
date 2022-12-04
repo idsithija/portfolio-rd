4a7878c07bb600f631673e153aa56fbf0da8c83f
idsithija <iidsithija@gmail.com>
2022-12-04 11:15:39 +0530

    smoth scroll

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -11,7 +11,7 @@ function Hero() {
           A copywriter who can help you create campaigns, ace pitches, and build great brands.
         </p>
       </div>
-      <div className="mt-6 h-full bg-hero-img bg-no-repeat shadow-1xbb">
+      <div className="mt-4 h-full bg-hero-img bg-no-repeat shadow-1xbb">
         <div className="p-3 mt-5 flex justify-center">
           <Image src={Person} width={250} alt="person" priority />
         </div>
