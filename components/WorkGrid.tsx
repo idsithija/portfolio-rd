177e55d8e8a2452cf266af9da025364a8cdf9ed1
idsithija <iidsithija@gmail.com>
2022-12-05 11:00:22 +0530

    changes

--- components/WorkGrid.tsx
+++ components/WorkGrid.tsx
@@ -1,10 +1,48 @@
 export default function WorkGrid() {
   return (
-    <div className="bg-white">
+    <div>
       <div className="pt-4 px-3 pb-6">
         <h3 className="text-[1.5rem]">
           Featured Work
         </h3>
+      </div>
+      <div className="flex flex-col space-y-1">
+        <div className="h-[400px] w-full bg-img-1 bg-no-repeat bg-cover shadow-1xbb">
+          <div className="p-3 flex flex-col justify-end h-full pb-10">
+            <h4 className="text-white text-[1.9rem]">Fashion Forward</h4>
+            <p className="text-white text-[1.5rem]">180fit</p>
+          </div>
+        </div>
+        <div className="h-[400px] w-full bg-img-2 bg-no-repeat bg-cover shadow-1xbb">
+          <div className="p-3 flex flex-col justify-end h-full pb-10">
+            <h4 className="text-white text-[1.9rem]">Fashion Forward</h4>
+            <p className="text-white text-[1.5rem]">180fit</p>
+          </div>
+        </div>
+        <div className="h-[400px] w-full bg-img-3 bg-no-repeat bg-cover shadow-1xbb">
+          <div className="p-3 flex flex-col justify-end h-full pb-10">
+            <h4 className="text-white text-[1.9rem]">Fashion Forward</h4>
+            <p className="text-white text-[1.5rem]">180fit</p>
+          </div>
+        </div>
+        <div className="h-[400px] w-full bg-img-4 bg-no-repeat bg-cover shadow-1xbb">
+          <div className="p-3 flex flex-col justify-end h-full pb-10">
+            <h4 className="text-white text-[1.9rem]">Fashion Forward</h4>
+            <p className="text-white text-[1.5rem]">180fit</p>
+          </div>
+        </div>
+        <div className="h-[400px] w-full bg-img-5 bg-no-repeat bg-cover shadow-1xbb">
+          <div className="p-3 flex flex-col justify-end h-full pb-10">
+            <h4 className="text-white text-[1.9rem]">Fashion Forward</h4>
+            <p className="text-white text-[1.5rem]">180fit</p>
+          </div>
+        </div>
+        <div className="h-[400px] w-full bg-img-6 bg-no-repeat bg-cover shadow-1xbb">
+          <div className="p-3 flex flex-col justify-end h-full pb-10">
+            <h4 className="text-white text-[1.9rem]">Fashion Forward</h4>
+            <p className="text-white text-[1.5rem]">180fit</p>
+          </div>
+        </div>
       </div>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,16 @@ module.exports = {
     extend: {
       backgroundImage: {
         "hero-img": "url('../assets/images/dan-gold-220226-1024x576.jpg')",
+        "img-1": "url('../assets/images/1.png')",
+        "img-2": "url('../assets/images/2.jpg')",
+        "img-3": "url('../assets/images/3.jpg')",
+        "img-4": "url('../assets/images/4.jpg')",
+        "img-5": "url('../assets/images/5.jpg')",
+        "img-6": "url('../assets/images/6.webp')",
       },
       boxShadow: {
         "1xbb": "0px 0px 0px 4000px rgba(27, 61, 88, 0.5) inset",
+        "1xcc": "0px 0px 0px 4000px rgb(80 80 80 / 51%) inset",
       },
     },
   },
