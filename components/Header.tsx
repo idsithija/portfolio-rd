c81bdb7b4ca0b4eff3a72a6df7869876cc21c5cd
idsithija <iidsithija@gmail.com>
2022-12-08 16:04:00 +0530

    desktop setup

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,13 +13,20 @@ function Header() {
   // }
 
   return (
-    <div className="flex p-3 justify-between items-center">
-      <div className="flex-[7_7_0%]">
-        <h1 className="text-[1.8rem]">Ranusha Darmarathne</h1>
+    <div className="flex p-3 lg:p-6 2xl:p-10 justify-between items-center">
+      <div>
+        <h1 className="text-[1.8rem] 2xl:text-[2.5rem]">Ranusha Darmarathne</h1>
       </div>
 
+      <div className="hidden lg:block">
+        <ul className="flex space-x-4 text-[1.8rem] text-[#2D2D2D]">
+          <li className="px-9 border border-[#2D2D2D] rounded-2xl hover:bg-[#fa793b]">Work</li>
+          <li className="px-9 border border-[#2D2D2D] rounded-2xl hover:bg-[#fa793b]">About</li>
+          <li className="px-9 border border-[#2D2D2D] rounded-2xl hover:bg-[#fa793b]">Contact</li>
+        </ul>
+      </div>
 
-      <div className="w-[30px] h-[30px] cursor-pointer">
+      <div className="w-[30px] h-[30px] cursor-pointer lg:hidden">
         <div className="relative w-[30px] h-[30px] ss">
           <span className="hambgurspan line1"></span>
           <span className="hambgurspan line3"></span>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,17 +3,17 @@ import Person from "../assets/images/SmilingMan.webp";
 
 function Hero() {
   return (
-    <div className="h-[650px] flex flex-col">
-      <div className="p-3">
-        <h2 className="text-[#161616] text-[14px] poppins-font">Consultant & Word Aficionado</h2>
-        <p className="mt-4 text-[#383838] font-semibold text-[18px] poppins-font">
-          Hello. I'm Daniel Mayer,
-          A copywriter who can help you create campaigns, ace pitches, and build great brands.
+    <div className="h-[650px] lg:h-[100%] flex flex-col">
+      <div className="p-3 lg:p-6 lg:h-[450px] 2xl:p-10">
+        <h2 className="text-[#161616] text-[14px] lg:text-[1.5rem] lg:mt-10 poppins-font">Consultant & Word Aficionado</h2>
+        <p className="mt-4 text-[#2D2D2D] font-semibold text-[18px] poppins-font lg:text-[3rem] lg:w-[600px] 2xl:w-[700px]">
+          Hey there!
+          I'm a digital and social media marketer based in Alberta, Canada.
         </p>
       </div>
-      <div className="mt-4 h-full bg-hero-img bg-no-repeat shadow-1xbb">
-        <div className="p-3 mt-5 flex justify-center">
-          <Image src={Person} width={230} alt="person" priority />
+      <div className="mt-4 h-full bg-hero-img bg-no-repeat shadow-1xbb lg:h-[500px] lg:relative 2xl:bg-cover">
+        <div className="p-3 2xl:p-10 mt-5 flex justify-center lg:absolute lg:top-[-50%] lg:px-6 lg:py-0 lg:mt-0 lg:right-0">
+          <Image className="2xl:w-[300px]" src={Person} width={230} alt="person" priority />
         </div>
       </div>
     </div>
